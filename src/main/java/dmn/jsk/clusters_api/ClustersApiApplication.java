61c7c53f2dee686e902e838c58c8fb17f55a415d
damian <damian.jasielczuk@gmail.com>
2018-06-26 22:03:58 +0200

    Initial Commit

---
+++ src/main/java/dmn/jsk/clusters_api/ClustersApiApplication.java
@@ -0,0 +1,12 @@
+package dmn.jsk.clusters_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClustersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClustersApiApplication.class, args);
+	}
+}
---
+++ src/test/java/dmn/jsk/clusters_api/ClustersApiApplicationTests.java
@@ -0,0 +1,16 @@
+package dmn.jsk.clusters_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClustersApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
